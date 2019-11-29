0ff7ea3392eda2dc33288ee99c25f1a14ec75d5f
Johnny Maravelis <johnny@phoenixafterglow.com>
2019-11-29 10:42:59 -0500

    correcting heroku deployment issues - incorrect port set up

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require("express");
 const path = require("path");
-const PORT = 3005;
+const PORT = process.env.PORT || 3005;
 const app = express();
 
 // Define middleware here
